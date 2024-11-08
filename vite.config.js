bffac97c5776a0212d7978769401c8c37802cbde
josue <josuealbelaez@gmail.com>
2024-11-08 18:46:05 -0300

    13

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL, 
+        target: import.meta.env.VITE_API_URL || 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
